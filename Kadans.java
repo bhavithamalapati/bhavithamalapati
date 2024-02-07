ead2d5896d419c46d78a518a02d7e8b489ea29a1
bhavithamalapati <159280126+bhavithamalapati@users.noreply.github.com>
2024-02-07 21:16:48 +0530

    Add files via upload

---
+++ Kadans.java
@@ -0,0 +1,18 @@
+public class Kadans {
+    public static void main(String[] args){
+        int arr[] = {-1,2,3,4,-2};
+        kadans(arr);
+    }
+    public static void kadans(int arr[]){
+        int currsum = 0;
+        int maxsum = 0;
+        for(int i=0;i<arr.length;i++){
+            currsum += arr[i];
+            if(currsum < 0){
+                currsum = 0;
+            }
+            maxsum = Math.max(maxsum,currsum);
+        }
+        System.out.println(maxsum);
+    }
+}
